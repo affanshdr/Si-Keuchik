db0984d68698950e30abbe7a8d3017bd203e30d0
Affan Suhendar <affansuhendar1@gmail.com>
2026-02-19 16:06:17 +0700

    piush

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["src/generated/**"],  // ← tambah ini
+  },
 ];
 
 export default eslintConfig;
